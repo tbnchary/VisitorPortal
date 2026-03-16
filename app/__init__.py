84076cb4c788340c0f4572d8f8c42f27829a8878
VMS Admin <admin@localhost>
2026-03-16 23:20:36 +0530

    added global error traceback to root out vercel 500

--- app/__init__.py
+++ app/__init__.py
@@ -13,4 +13,10 @@ def create_app(config_class=Config):
     importlib.import_module("app.fleet_routes")
     app.register_blueprint(main_bp)
 
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+        import traceback
+        print("Encountered exception: ", e) # log to vercel functions
+        return "<pre><h2>Flask Application Error:</h2><br>" + traceback.format_exc() + "</pre>", 500
+
     return app
